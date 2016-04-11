dd4a3c1eb2cd81c5bbed38330411ab0b5816c312
filippo lenzi <filippo lenzi>
2016-04-11 15:06:03 +0200

    Updated mod name.

--- src/java/com/filloax/improvedcommands/Main.java
+++ src/java/com/filloax/improvedcommands/Main.java
@@ -17,7 +17,7 @@ import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class Main {
 
     public static final String MODID = "improvedcommands";
-    public static final String MODNAME = "Improved Vanilla Commands";
+    public static final String MODNAME = "Improved Commands";
     public static final String VERSION = "1.0";
         
     @Instance
